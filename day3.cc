b97e21bc82fa59b880d4326496f6d2e1c6a29d8a
TaoJun724 <2086239852@qq.com>
2019-06-29 15:06:24 +0800

    day day up

---
+++ day3.cc
@@ -0,0 +1,43 @@
+//数据流中的中位数
+
+class Solution {
+    private:
+        vector<int> min; //数组中的后一半元素组成一个最小化堆
+        vector<int> max; //数组中的前一半元素组成一个最大化堆
+    public:
+        void Insert(int num) {
+            if(((min.size()+max.size()) & 1) == 0) {  //偶数数据的情况下，则在最小堆中插入元素
+                if(max.size() > 0 && num < max[0]) {
+                    max.push_back(num);
+                    push_heap(max.begin(), max.end(), less<int>());
+                    num=max[0];
+                    pop_heap(max.begin(), max.end(), less<int>());
+                    max.pop_back();
+                }
+                min.push_back(num); //把前一半找到的最大值放到后一半中
+                push_heap(min.begin(), min.end(), greater<int>());
+            } else {
+                if(min.size() > 0 && num > min[0]) {   //奇数数据的情况下，则在最大堆中插入元素
+                    min.push_back(num);
+                    push_heap(min.begin(), min.end(), greater<int>());
+                    num=min[0];
+                    pop_heap(min.begin(), min.end(), greater<int>());
+                    min.pop_back(); 
+                }
+                max.push_back(num); //把后一半找到的最大值放到前一半中
+                push_heap(max.begin(), max.end(), less<int>());
+            }
+        }
+ 
+        double GetMedian() { 
+            int size=min.size() + max.size();
+            if(size==0) return -1;
+            double median = 0;
+            if((size&1) != 0) {
+                median = (double) min[0];
+            } else {
+                median = (double) (max[0] + min[0]) / 2;
+            }
+            return median;
+        }
+};
